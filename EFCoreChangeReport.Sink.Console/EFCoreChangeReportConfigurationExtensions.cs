3cc212659c8afa26a4039aaa829dc96d3243659b
Sulexa <sulexa95@gmail.com>
2019-06-30 01:08:43 +0200

    Getting started

---
+++ EFCoreChangeReport.Sink.Console/EFCoreChangeReportConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreChangeReport.Configuration;
+using EFCoreChangeReport.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace EFCoreChangeReport.Sink.Console
+{
+    public static class EFCoreChangeReportConfigurationExtensions
+    {
+        public static IEFCoreChangeReportConfiguration ToConsole(this IEFCoreChangeReportConfiguration databaseChangeReportConfiguration)
+        {
+            if (databaseChangeReportConfiguration == null) throw new ArgumentNullException(nameof(databaseChangeReportConfiguration));
+
+            databaseChangeReportConfiguration.SinkReports += SinkReportsToConsole;
+
+            return databaseChangeReportConfiguration;
+        }
+
+        private static void SinkReportsToConsole(List<Report> reports)
+        {
+            foreach (var report in reports)
+            {
+                System.Console.WriteLine(JsonConvert.SerializeObject(report));
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# EFCoreChangeReport.Sink.Console
+# EFCoreChangeReport.Sink.Console ![Nuget](https://img.shields.io/nuget/v/EFCoreChangeReport.Sink.Console.svg?style=flat) ![GitHub last commit](https://img.shields.io/github/last-commit/Sulexa/EFCoreChangeReport.Sink.Console.svg?style=flat)
+
+## Description
+
+A sink for [EFCoreChangeReport](https://github.com/Sulexa/EFCoreChangeReport/) to write in the console all the reports
